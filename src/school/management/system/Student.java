6b8d3126da625eeb5fede3c7d03897d790f0a062
Ferhat Demir <dmrfrht1@gmail.com>
2020-11-08 00:05:28 +0300

    first java app

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,10 @@
+package school.management.system;
+
+/**
+ * This class responsible for keeping the
+ * track of students fees, name, grade, fees paid
+ */
+public class Student {
+  private int id;
+  private  String name;
+}
